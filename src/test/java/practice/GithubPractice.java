6015f0768f5d66680b3b9797a53f0ebb497838e9
sachi <sachi@PureBook-S14>
2023-04-28 13:00:07 +0530

    ok

--- src/test/java/practice/GithubPractice.java
+++ src/test/java/practice/GithubPractice.java
@@ -4,6 +4,11 @@ public class GithubPractice {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("hello");
+		System.out.println("kempu");
+		System.out.println("ss");
+		System.out.println("danger");
+		System.out.println("");
+		System.out.println("taqat");
 	}
 
 }
